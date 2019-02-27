b2d2fc9ce3d67d07d5e7ddd3cf531459195735c9
yurii-p-apiko <y.pelekhatyi@apiko.com>
2019-02-27 15:30:43 +0200

    small api fix

--- src/api/Api.js
+++
@@ -1,9 +0,0 @@
-import { AsyncStorage } from 'react-native';
-
-export const setUser = async (user) => {
-  try {
-    await AsyncStorage.getItem('user', user);
-  } catch (error) {
-    console.log('ERROR: SET USER: ', error);
-  }
-};
