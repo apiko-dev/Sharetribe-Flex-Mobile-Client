3785de841b5f11b0edae053d977462c6805081ea
t-perun <t.perun@apiko.com>
2019-05-27 14:53:46 +0300

    change RatingTable

--- src/components/ReviewsList/components/RatingTable/RatingTable.js
+++ src/components/ReviewsList/components/RatingTable/RatingTable.js
@@ -6,54 +6,30 @@ import s from './styles';
 
 function RatingTable({ ratings, averageRating }) {
   const checkRating = isNaN(averageRating) ? 0 : averageRating;
-  const normalizeRating = ratings.reduce(
-    (acc, current) => {
-      try {
-        switch (current) {
-          case 1:
-            acc.one.push(current);
-            acc.total += 1;
-            break;
-          case 2:
-            acc.two.push(current);
-            acc.total += 1;
-            break;
-          case 3:
-            acc.three.push(current);
-            acc.total += 1;
-            break;
-          case 4:
-            acc.four.push(current);
-            acc.total += 1;
-            break;
-          case 5:
-            acc.five.push(current);
-            acc.total += 1;
-            break;
-          default:
-            return acc;
-        }
-        return acc;
-      } catch (err) {
-        console.log(err);
-      }
-    },
-    {
-      one: [],
-      two: [],
-      three: [],
-      four: [],
-      five: [],
-      total: 0,
-    },
-  );
-  function ratingCalculate(arr, total) {
+
+  const total = ratings.length;
+  const normalizeRating = ratings.reduce((acc, current) => {
+    const index = current - 1;
+    const arrayItem = acc[index];
+
+    if (Array.isArray(arrayItem)) {
+      acc[index].push(current);
+    } else {
+      acc[index] = [current];
+    }
+
+    return acc;
+  }, Array(5));
+
+  function ratingCalculate(arr) {
+    if (!arr) {
+      return '0%';
+    }
+
     const calculate = (100 * arr.length) / total;
 
     return `${calculate}%`;
   }
-
-  const { one, two, three, four, five, total } = normalizeRating;
 
   return (
     <View style={s.container}>
@@ -67,61 +43,19 @@ function RatingTable({ ratings, averageRating }) {
         />
       </View>
       <View style={s.tableContainer}>
-        <View style={s.lineContainer}>
-          <Text style={s.numberLine}>1</Text>
-          <View style={s.line}>
-            <View
-              style={[
-                s.lineContent,
-                { width: ratingCalculate(one, total) },
-              ]}
-            />
+        {normalizeRating.map((item, index) => (
+          <View style={s.lineContainer} key={index}>
+            <Text style={s.numberLine}>{index + 1}</Text>
+            <View style={s.line}>
+              <View
+                style={[
+                  s.lineContent,
+                  { width: ratingCalculate(item) },
+                ]}
+              />
+            </View>
           </View>
-        </View>
-        <View style={s.lineContainer}>
-          <Text style={s.numberLine}>2</Text>
-          <View style={s.line}>
-            <View
-              style={[
-                s.lineContent,
-                { width: ratingCalculate(two, total) },
-              ]}
-            />
-          </View>
-        </View>
-        <View style={s.lineContainer}>
-          <Text style={s.numberLine}>3</Text>
-          <View style={s.line}>
-            <View
-              style={[
-                s.lineContent,
-                { width: ratingCalculate(three, total) },
-              ]}
-            />
-          </View>
-        </View>
-        <View style={s.lineContainer}>
-          <Text style={s.numberLine}>4</Text>
-          <View style={s.line}>
-            <View
-              style={[
-                s.lineContent,
-                { width: ratingCalculate(four, total) },
-              ]}
-            />
-          </View>
-        </View>
-        <View style={s.lineContainer}>
-          <Text style={s.numberLine}>5</Text>
-          <View style={s.line}>
-            <View
-              style={[
-                s.lineContent,
-                { width: ratingCalculate(five, total) },
-              ]}
-            />
-          </View>
-        </View>
+        ))}
       </View>
     </View>
   );
